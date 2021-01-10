9b6ba6bb6733046f2fd0189a68507897ffd99350
mhshahib <mh.shahib@gmail.com>
2021-01-10 18:06:59 -0500

    Hello Java is added

--- src/advance/Test.java
+++ src/advance/Test.java
@@ -9,8 +9,9 @@ public class Test {
 	public static void main(String[] args) {
 		//System.out.println(LearnDataType.mobileBrand);
 		
-		String studentName ="Cherif Ameur";
+		
 		System.out.println("Hello Mr Mamun");
+		System.out.println("Hello Java");
 		
 		
 		
